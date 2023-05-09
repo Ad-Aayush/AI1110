990939c590c9f3f05ed9b955c2b54ac38deb4ec8
Aayush Adlakha <aayushadlakha15@gmail.com>
2023-05-09 21:18:36 +0530

    Draft

---
+++ Assignment_4/codes/norm.py
@@ -0,0 +1,17 @@
+from scipy.stats import norm
+from scipy.stats import binom
+import numpy as np
+import matplotlib.pyplot as plt
+
+p = 0.9
+n = 10
+k = 6
+sig = np.sqrt(n*p*(1-p))
+
+xpoints = np.linspace(0,10,11)
+ypoints = np.array(binom.cdf(xpoints,n,p) )
+plt.stem(xpoints, ypoints,linefmt='r-', markerfmt='ro', basefmt='k--')
+xpoints = np.linspace(0,10,10000)
+ypoints = np.array(norm.cdf((xpoints-n*p+0.5)/sig))
+plt.plot(xpoints, ypoints,'b')
+plt.show() 
